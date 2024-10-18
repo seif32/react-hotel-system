3e9c3a52815a2f09f98a7ac76443a06a329480bc
Seif Mohamed <20p1710@eng.asu.edu.eg>
2024-10-18 17:40:50 +0300

    Add Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Roomify</Heading>
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Button>Check In</Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests..." />
+              <Input type="number" placeholder="Number of guests..." />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
