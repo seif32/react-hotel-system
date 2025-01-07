43622c456b48f321599eaa56146387c4d889c149
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-01-07 16:31:46 +0200

    Add log-out button

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,12 @@
+import { HiArrowRightEndOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+
+function Logout() {
+  return (
+    <ButtonIcon>
+      <HiArrowRightEndOnRectangle />
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
