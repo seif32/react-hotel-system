b1696f19eedff7bfccdd81da0bc5c16bc7a9a980
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-01-07 10:17:18 +0200

    Add check-out for each booking

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,8 +7,9 @@ import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
-import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -52,6 +53,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { checkout, isCheckout } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -102,6 +104,16 @@ function BookingRow({
               Check in
             </Menus.Button>
           )}
+
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckout}
+            >
+              Check out
+            </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
@@ -109,3 +121,7 @@ function BookingRow({
 }
 
 export default BookingRow;
+
+// checkout
+
+// 1.
